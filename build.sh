82a4809587d5336fd517344802e2968e91af2e43
SatohJohn <sato.keita@furyu.jp>
2022-06-24 22:13:59 +0900

    editable image path

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ set -euf -o pipefail
 
 function usage(){
     echo >&2
-    echo "Usage: $0 image_name project" >&2
+    echo "Usage: $0 image_name project region" >&2
     echo "Builds this service container in Google Container Registry (gcr.io)." >&2
     echo >&2
     echo "image_name is optional. default value is gcs-streaming-proxy." >&2
@@ -24,13 +24,19 @@ function usage(){
     echo "project is optional; your gcloud config project will be used if it" >&2
     echo "is not provided." >&2
     echo >&2
+    echo "base is optional; default value is gcr.io (Google Container Registry) if it" >&2
+    echo "is not provided. When you want to use Google Artifact Registry" >&2
+    echo "set value docker.pkg.dev." >&2
+    echo >&2
 }
 
 IMAGE_NAME=${1-}
 IMAGE_NAME="${1:-gcs-streaming-proxy}"
 PROJECT=${2-}
 PROJECT="${2:-$(gcloud config get-value project)}"
-TAG=gcr.io/"${PROJECT}"/"${IMAGE_NAME}"
+BASE=${3-}
+BASE=${3:-gcr.io}
+TAG="${BASE}"/"${PROJECT}"/"${IMAGE_NAME}"
 
 # quick and dirty way to catch if the user asks for help, like --help
 # downside: you can't tag the image as *help or just "-h"
